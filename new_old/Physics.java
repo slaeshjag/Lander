36eef56a3f93ba1b42c9a6265d77d5728c0be2f0
Steven Arnow <s@rdw.se>
2013-02-07 22:53:34 +0100

    Removed old crap

--- new_old/Physics.java
+++
@@ -1,9 +0,0 @@
-public class Physics {
-	float		gravity;
-	float		terminal_velocity;
-
-	Physics(float gravity, float terminal_velocity) {
-		this.gravity = gravity;
-		this.terminal_velocity = terminal_velocity;
-	}
-}
